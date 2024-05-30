cc1bd43b320df5e6044e98716a87db68ce51cf06
fpungo <60239261+fpungo@users.noreply.github.com>
2024-05-30 15:55:55 -0500

    MODIFICACIONES MONITORING

--- measurements/monitoring/settings.py
+++ measurements/monitoring/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'monitoring.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'monitoring', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
