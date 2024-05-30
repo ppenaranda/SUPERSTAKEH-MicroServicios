9cdc5b50776cc832a2f4eb002f8985bba0a2fab1
fpungo <60239261+fpungo@users.noreply.github.com>
2024-05-30 16:11:36 -0500

    cambioMeasurements

--- measurements/measurements/models.py
+++ measurements/measurements/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+import hashlib
 
 class Measurement(models.Model):
-    variable = models.IntegerField(null=False, default=None)
-    value = models.FloatField(null=True, blank=True, default=None)
-    unit = models.CharField(max_length=50)
-    place = models.CharField(max_length=50)
-    dateTime = models.DateTimeField(auto_now_add=True)
-
+    trabajo = models.CharField(max_length=255, default='')
+    ingresos = models.CharField(max_length=200, default='')
+    deudas = models.CharField(max_length=200, default='')
+    creditos = models.CharField(max_length=200, default='')
+    
     def __str__(self):
-        return '%s %s' % (self.value, self.unit)
+        return '{}'.format(self.trabajo)
+
+    def hashear(self, valor):
+        hash = hashlib.md5((valor).encode()).hexdigest()
+        return hash
+
+    def save(self, *args, **kwargs):
+        self.trabajo = self.hashear(self.trabajo)
+        self.ingresos = self.hashear(self.ingresos)
+        self.deudas = self.hashear(self.deudas)
+        self.creditos = self.hashear(self.creditos)
+        super(Measurement, self).save(*args, **kwargs)
